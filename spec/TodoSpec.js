2ab81d99b2d06a8b073a5a6da6df9d26c1a7b6b5
Hack Reactor Students <accounts@hackreactor.com>
2013-03-30 12:28:52 -0700

    Added specs

---
+++ spec/TodoSpec.js
@@ -0,0 +1,13 @@
+describe("Todo", function() {
+  var Todo;
+  beforeEach(function() {
+    todoData = {
+      completed: false,
+      text: 'This task is not completed'
+    }
+    Todo = new Todo(todoData);
+  });
+  it("has the mandatory properties", function() {
+    
+  });
+})
